44776639a5b3b0b4a58ce31e7c86fa4bbc7b1254
papisko7 <kkowalski0477@gmail.com>
2025-11-26 11:41:37 +0100

    Update

---
+++ Backend/ShoppingList.Data/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace ShoppingList.Data.Entities
+{
+	public class User
+	{
+		public int ID { get; set; }
+
+		public string Username { get; set; }
+
+		public string PasswordHash { get; set; }
+
+		public DateTime CreatedAt { get; set; }
+
+		public ICollection<Token> Tokens { get; set; } = new List<Token>();
+	}
+}
