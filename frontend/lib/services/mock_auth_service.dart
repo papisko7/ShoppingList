afe675404d5094467f52affe8c7119543e431da5
Jakub Bromber <66789194+Koba3000@users.noreply.github.com>
2025-12-03 12:28:09 +0100

    change email to username

--- frontend/lib/services/mock_auth_service.dart
+++ frontend/lib/services/mock_auth_service.dart
@@ -1,16 +1,16 @@
 class MockAuthService {
   final Map<String, String> _users = {'test@example.com': 'password123'};
 
-  Future<bool> login(String email, String password) async {
+  Future<bool> login(String username, String password) async {
     await Future.delayed(const Duration(seconds: 1)); // Simulate network delay
-    return _users[email] == password;
+    return _users[username] == password;
   }
 
-  Future<bool> register(String email, String password) async {
+  Future<bool> register(String username, String password) async {
     await Future.delayed(const Duration(milliseconds: 500));
-    if (_users.containsKey(email)) return false;
+    if (_users.containsKey(username)) return false;
 
-    _users[email] = password;
+    _users[username] = password;
     return true;
   }
 }
