cf528a970b3aba6ac4e1ad4650f013383f436b62
kkowa <kkowa@EREDAN>
2025-12-23 22:45:32 +0100

    Implemented final logic to groups and synchronisation. Formated the code, improved upon the structure, made final tweaks. Backend side is done.

---
+++ Backend/ShoppingList.Data/Migrations/20251223213644_ShoppingListEntityUpdate.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingList.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShoppingListEntityUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingLists_Groups_GroupEntityId",
+                table: "ShoppingLists");
+
+            migrationBuilder.RenameColumn(
+                name: "GroupEntityId",
+                table: "ShoppingLists",
+                newName: "GroupId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ShoppingLists_GroupEntityId",
+                table: "ShoppingLists",
+                newName: "IX_ShoppingLists_GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingLists_Groups_GroupId",
+                table: "ShoppingLists",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingLists_Groups_GroupId",
+                table: "ShoppingLists");
+
+            migrationBuilder.RenameColumn(
+                name: "GroupId",
+                table: "ShoppingLists",
+                newName: "GroupEntityId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ShoppingLists_GroupId",
+                table: "ShoppingLists",
+                newName: "IX_ShoppingLists_GroupEntityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingLists_Groups_GroupEntityId",
+                table: "ShoppingLists",
+                column: "GroupEntityId",
+                principalTable: "Groups",
+                principalColumn: "Id");
+        }
+    }
+}
