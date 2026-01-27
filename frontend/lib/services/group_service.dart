381135fa8886e0d8d4533183029ef91d0834c345
Jakub Bromber <66789194+Koba3000@users.noreply.github.com>
2026-01-27 02:38:11 +0100

    adding groups

--- frontend/lib/services/group_service.dart
+++ frontend/lib/services/group_service.dart
@@ -23,4 +23,25 @@ class GroupService {
     final List data = jsonDecode(res.body);
     return data.map((group) => Group.fromJson(group)).toList();
   }
+
+  Future<Group> createGroup(String name) async {
+    print('[GROUP SERVICE] createGroup name="$name"');
+
+    final res = await api.post(
+      '/api/Groups/create-group',
+      body: {'name': name},
+    );
+
+    print('[GROUP SERVICE] createGroup status: ${res.statusCode}');
+    if (res.statusCode != 200 && res.statusCode != 201) {
+      print('[GROUP SERVICE] createGroup body: ${res.body}');
+      throw Exception('Failed to create group');
+    }
+
+    final json = jsonDecode(res.body) as Map<String, dynamic>;
+    final created = Group.fromJson(json);
+
+    print('[GROUP SERVICE] created group id=${created.id}');
+    return created;
+  }
 }
